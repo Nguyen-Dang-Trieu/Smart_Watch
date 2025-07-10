486da6ad3f17eed0d625abeedf1008616b023a46
Nguyen dang trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2025-07-10 22:30:31 +0700

    Update TTP226.h

--- Sensor_Driver/TTP226/TTP226.h
+++ Sensor_Driver/TTP226/TTP226.h
@@ -1,1 +1,15 @@
+#ifndef TTP226_H
+#define TTP226_H
 
+#include <stdint.h>
+#include "stm32f1xx_ll_rcc.h"
+#include "stm32f1xx_ll_gpio.h"
+/*
+ * DO - PA0, CLK - PA1, RST - PA2, DV - PA3.
+ */
+
+void TTP226_Init();
+void TTP226_Config(uint32_t DO, uint32_t CLK, uint32_t RST, uint32_t DV);
+uint8_t TTP226_Read();
+
+#endif // TTP226_h
