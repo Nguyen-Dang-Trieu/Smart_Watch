fb8a5d40f691b38ba8e8465bd1171b159497d155
Nguyen dang trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2025-07-10 23:31:06 +0700

    Update TTP226.c

--- Sensor_Driver/TTP226/TTP226.c
+++ Sensor_Driver/TTP226/TTP226.c
@@ -13,14 +13,14 @@ void TTP226_Config(void)
 
   /*     GPIO Configiratopm     */  
   /* PA0 (DO) | PA3 (DV) - Input pull-up */
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_0 | LL_GPIO_PIN_3;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  GPIO_InitStruct.Pin     = LL_GPIO_PIN_0 | LL_GPIO_PIN_3;
+  GPIO_InitStruct.Mode    = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull    = LL_GPIO_PULL_UP;
   LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* PA1 (CLK) | PA2 (RST) - Output push-pull */
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_1 | LL_GPIO_PIN_2;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Pin        = LL_GPIO_PIN_1 | LL_GPIO_PIN_2;
+  GPIO_InitStruct.Mode       = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Speed      = LL_GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStruct.Pull       = LL_GPIO_PULL_NO;
