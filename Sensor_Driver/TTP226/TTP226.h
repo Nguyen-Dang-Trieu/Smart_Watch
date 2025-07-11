97a5c210aefdbb6c03e778fb543f909c8941bdc2
Nguyen dang trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2025-07-11 17:36:13 +0700

    Update TTP226.h

--- Sensor_Driver/TTP226/TTP226.h
+++ Sensor_Driver/TTP226/TTP226.h
@@ -2,11 +2,7 @@
 #define TTP226_H
 
 #include <stdint.h>
-#include "stm32f1xx_ll_rcc.h"
-#include "stm32f1xx_ll_gpio.h"
-#include "stm32f1xx_ll_exti.h"
-#include "stm32f1xx_ll_system.h"
-#include "stm32f1xx_ll_utils.h"    // LL_mDelay()
+#include "stm32f1xx.h"
 
 /*
  * DO - PA0, CLK - PA1, RST - PA2, DV - PA3.
